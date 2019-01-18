79afa11949ee7872f5400d79ff6c99f0b3a99ff7
Rene <armenta_j@ymail.com>
2019-01-17 21:06:38 -0700

    made table work

--- Unidad.php
+++ Unidad.php
@@ -1,51 +1,60 @@
-<?php
+<?php include '../Horarios2.0/assets/php/header.php'?>
+   <html>    
+       <head>
+        <title>Horarios</title>
 
-?>
+        <link rel="stylesheet" href="../Horarios2.0/assets/css/estilos.css">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+        
+        
 
-<html>
-     <head>
-        <title>Unidades</title>
-
-        <link rel="stylesheet" href="../assets/css/estilos.css">
+        
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="
             stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../assets/css/lib/animate.css">
+        <link rel="stylesheet" href="../Horarios2.0/assets/css/lib/animate.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="../assets/js/lib/sweetalert2.js"></script>
-        
+        <script src="../Horarios2.0/assets/js/lib/sweetalert2.js"></script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        
-         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </head>
-    
     <body>
-        <table id="table_id" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-        </tr>
-        <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-        </tr>
-    </tbody>
-</table>
+        <?php get_header();?>
+         
+        <div class="container">
+         <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Direccion</th>
+                <th>Director</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+                <th><button type="button" class="btn btn-danger">Eliminar</button></th>
+            </tr>
+        </tbody>
+        </table>
+        </div>
+        <?php get_footer();?>
     </body>
 <script>
     $(document).ready(function() {
-    $('#table_id').DataTable();
+    $('#example').DataTable();
 } );
 </script>
 </html>
--- views/categoria.php
+++ views/categoria.php
@@ -1,16 +1,16 @@
-<?php include '../assets/php/header.php'?>
+<?php include '../Horarios2.0/assets/php/header.php'?>
    <html>
     <head>
         <title>Horarios</title>
 
-        <link rel="stylesheet" href="../assets/css/estilos.css">
+        <link rel="stylesheet" href="./Horarios2.0/assets/css/estilos.css">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="
             stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/css/lib/animate.css">
+        <link rel="stylesheet" href="../Horarios2.0/assets/css/lib/animate.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="../assets/js/lib/sweetalert2.js"></script>
+        <script src="../Horarios2.0/assets/js/lib/sweetalert2.js"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
   
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
